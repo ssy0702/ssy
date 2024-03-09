342109d2834cbc58d9f9f992b028e681e17ef5ff
ssy0702 <ssy0702sjy@gmail.com>
2024-03-09 12:45:35 +0900

    2024-03-09 성승엽

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#pragma warning(disable:4996)
+
+/*
+int main(void)
+{
+	int num1 = 10;
+	int num2 = 12;
+	int result1, result2, result3;
+
+	result1 = (num1 == 10 && num2 == 12);
+	result2 = (num1 < 12 || num2>12);
+	result3 = (!num1);
+
+	printf("result1: %d \n", result1);
+	printf("result2: %d \n", result2);
+	printf("result3: %d \n", result3);
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int num1 = 1, num2 = 2;
+	printf("Hello "), printf("World! \n");
+	num1++, num2++;
+	printf("%d ", num1), printf("%d ", num2), printf("\n");
+	return 0;
+}*/
+
+/*int main(void)
+{
+	int result;
+	int num1, num2;
+
+	printf("정수 one: ");
+	scanf_s("%d", &num1);
+		printf("정수 two: ");
+	scanf_s("%d", &num2);
+
+	result = num1 + num2;
+	printf("%d + %d = %d \n", num1, num2, result);
+	return 0;
+}
+*/
+ /*int main(void)
+{
+	int result;
+	int num1, num2, num3;
+
+	printf("세 개의 정수 입력: ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	result = num1 + num2 + num3;
+	printf("%d + %d + %d = %d \n", num1, num2, num3, result);
+	return 0;
+
+}
+*/
+int main(void)
+{
+	int result, result2;
+	int num1, num2;
+
+	printf("정수 one: ");
+	scanf_s("%d", &num1);
+	printf("정수 two: ");
+	scanf_s("%d", &num2);
+
+	result = num1 - num2;
+	printf("%d - %d = %d \n", num1, num2, result);
+	result2 = num1 * num2;
+	printf("%d * %d = %d \n", num1, num2, result2);
+	return 0;
+	
+
+}
